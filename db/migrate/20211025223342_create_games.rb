ef19e60a6e9e05c5a76c7f10528a1cd03d1de134
WilliamPerron1 <william-la@hotmail.com>
2021-10-27 14:19:00 -0400

    avancer migration

--- db/migrate/20211025223342_create_games.rb
+++ db/migrate/20211025223342_create_games.rb
@@ -1,7 +1,11 @@
 class CreateGames < ActiveRecord::Migration[6.1]
   def change
     create_table :games do |t|
-
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :user, foreign_key: true
+      t.text :game_data
       t.timestamps
     end
   end
