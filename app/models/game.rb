34e63f5551f2ba05620cb90624aad016415ce634
WilliamPerron1 <william-la@hotmail.com>
2021-11-15 12:03:05 -0500

    avancement

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,5 @@
 class Game < ApplicationRecord
-    has_many : users
+    has_many :users
+
+    validates :creator_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+    has_many :games
+
+    validates :email, presence: true, uniqueness:true
+    validates :encrypted_password, presence: true
+
     
 end
