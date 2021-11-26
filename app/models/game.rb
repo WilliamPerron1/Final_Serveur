4998cd919797e5f1028d1eac1e3577796ff2fe06
WilliamPerron1 <william-la@hotmail.com>
2021-11-26 15:43:45 -0500

    model

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,11 @@ class Game < ApplicationRecord
 
     validates :creator_id, presence: true
 
+    belong_to :creator, class_name 'user', foreign_key 'creator_id'
+    belong_to :opponent, class_name 'user', foreign_key 'opponent_id', optional: true
+    belong_to :winner, class_name 'user', foreign_key 'winner_id', optional: true
+    belong_to :player_turn, class_name 'user', foreign_key 'player_turn_id', optional: true
+
 
     # les relations son pas bonne
     #belong_to 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    has_many :games
+    has_many :created, class_name: 'game', foreign_key: 'creator_id'
+    has_many :played, class_name: 'game', foreign_key: 'opponent_id'
 
     #has many mais pointe vers les foreign key exemple createur id pour avoir les games que ta creer
     #meme chose mais aussi avec opponent id pour avoir les game que tetais le opponent
